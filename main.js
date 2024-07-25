36449e3e383bf98778ecc0c6443f6fcd144cf789
lekston <lekstonjm@gmail.com>
2024-07-25 09:53:31 +0200

    log key

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ window.onload = () => {
       alert('please enter a metered id');
     }
     const key = urlSearchParams.get('metered_id');
+    alert(key);
     async function loadIceServers() {
       const response = await fetch(
         "https://your-app-name.metered.live/api/v1/turn/credentials?apiKey=" +
